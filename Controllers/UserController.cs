bd969fd1b7325e846a1e9947d91355f66306aebd
Simran Swarnkar <simranswarnkar@gmail.com>
2026-01-21 21:32:19 +0530

    Removed home pages and added user & vendor modules

---
+++ Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NUTRIBITE.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/VendorController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NUTRIBITE.Controllers
+{
+    public class VendorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Vendor controller is working");
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+      /*  public IActionResult Dashboard()
+        {
+            return View();
+        }*/
+    }
+}
